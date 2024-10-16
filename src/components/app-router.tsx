5ca449a7f811303618105f2d78dcc1d3f38b9f49
hayk-zohrabyan <hzohrabyan@sol-dynamis.com>
2024-10-16 17:20:24 +0400

    first commit

---
+++ src/components/app-router.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { routes } from '../routes';
+
+const AppRouter = () => {
+  return (
+    <Routes>
+      {routes.map(r =>
+        <Route
+          key={r.path}
+          path={r.path}
+          element={<r.element/>}
+        />
+      )}
+    </Routes>
+  );
+};
+
+export default AppRouter;
